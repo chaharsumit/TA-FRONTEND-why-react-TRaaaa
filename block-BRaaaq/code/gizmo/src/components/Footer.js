1af1022af16d293edfb96c9dc3bfb844a69cfaca
Sumit Chahar <chahar.sumit888@gmail.com>
2022-01-17 22:06:25 +0530

    Add propType checks for data keys

---
+++ block-BRaaaq/code/gizmo/src/components/Footer.js
@@ -0,0 +1,18 @@
+function Footer(){
+  return (
+    <footer className="footer">
+      <div className="container">
+        <nav className="footer-nav">
+          <small>2020 &copy; All Rights Reserved.</small>
+          <ul className="footer-menu">
+            <li className="footer-menu-item">F</li>
+            <li className="footer-menu-item">T</li>
+            <li className="footer-menu-item">I</li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
